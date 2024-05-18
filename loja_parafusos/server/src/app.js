8917bc8b490d4684d65e69c3cc5c3081acbd6b30
Pedro Henrique <pedrom.pepeu@gmail.com>
2024-05-17 21:44:02 -0300

    crud completo

---
+++ loja_parafusos/server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const Usuariocontroller = require("./controllers/UsuarioController")
+const port = 5000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get("/usuarios", Usuariocontroller.read)
+app.post("/usuarios", Usuariocontroller.create)
+app.put("/usuarios/:id_usuario", Usuariocontroller.update)
+app.delete("/usuarios/:id_usuario", Usuariocontroller.delete)
---
+++ loja_parafusos/server/src/controllers/UsuarioController.js
@@ -0,0 +1,74 @@
+const Usuario = require("../models/Usuarios")
+
+class UsuarioController{
+    //Criando crud
+    create(req,res){
+        // let nome = req.body.nome
+        // let login = req.body.login
+        // let sehna = req.body.senha
+
+        let {nome,login,senha} = req.body
+
+        Usuario.inserirUsuario(nome, login, senha).then(
+            resposta=>{
+                console.debug("criando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        ).catch(
+            resposta=>{
+                console.debug("Erro: criando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        )
+    }
+
+    read(req,res){
+        Usuario.mostarUsuarios().then(
+            resposta=>{
+                console.debug("Mostrando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        ).catch(
+            resposta=>{
+                console.debug("Erro: Mostrando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        )
+    }
+
+    update(req,res){
+        let {id_usuario} = req.params
+        let {nome,login,senha} = req.body
+
+        Usuario.atualizarUsuario(id_usuario, nome, login, senha).then(
+            resposta=>{
+                console.debug("atualizando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        ).catch(
+            resposta=>{
+                console.debug("Erro: atualizando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        )
+    }
+
+    delete(req, res){
+        let {id_usuario} = req.params
+        
+
+        Usuario.deletarUsuario(id_usuario).then(
+            resposta=>{
+                console.debug("deletando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        ).catch(
+            resposta=>{
+                console.debug("Erro: deletando usuarios")
+                res.status(resposta[0]).json(resposta[1])
+            }
+        )
+    }
+}
+
+module.exports = new UsuarioController()
